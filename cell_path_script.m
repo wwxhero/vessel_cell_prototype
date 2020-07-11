253b06e47167596ef32d1e0f431768c92b93e68f
wanxwang <wanxin-wang@uiowa.edu>
2020-07-11 18:45:14 -0500

    done prototyping a segment of vessel and the path of cell trajectory based on geometry

--- cell_path_script.m
+++ cell_path_script.m
@@ -7,7 +7,7 @@ pp_a_x = spline(s_spec, alpha_x_spec);
 pp_a_y = spline(s_spec, alpha_y_spec);
 pp_a_z = spline(s_spec, alpha_z_spec);
 pp_b   = spline(s_spec, bata_spec);
-s 			 = 0:0.025:7;
+s 			 = 0:0.025:6.999;
 
 alpha_x = ppval(pp_a_x, s);
 alpha_y = ppval(pp_a_y, s);
@@ -18,7 +18,7 @@ rk		= bata .* abs(cos(s/2));
 rk = rk - 0.01;
 thetak	= 2 * s;
 
-[~, N_alpha, B_alpha] = ff_spline(pp_a_x, pp_a_y, pp_a_z, s_spec, s);
+[T_alpha, N_alpha, B_alpha] = ff_spline(pp_a_x, pp_a_y, pp_a_z, s_spec, s);
 
 p_x 	= alpha_x + rk .* cos(thetak) .* N_alpha(1) + rk .* sin(thetak) .* B_alpha(1);
 p_y 	= alpha_y + rk .* cos(thetak) .* N_alpha(2) + rk .* sin(thetak) .* B_alpha(2);
@@ -41,10 +41,47 @@ for i_s = 1 : n_s
 	err(1, i_s) = (bata(1, i_s) - d_alpha_p(1, i_s)); %make sure the cell is in vessel
 end
 
+phi = 0:0.1:2*pi+0.1;
+[S,PHI] = meshgrid(s, phi);
+[m, n] = size(S);
+T = zeros(m, n, 3);
+for i_phi = 1 : m
+	for i_s = 1 : n
+		T(i_phi, i_s, :) = alpha_s(:, i_s) ...
+						+ bata(1, i_s) * cos(phi(1, i_phi)) * N_alpha(:, i_s) ...%
+						+ bata(1, i_s) * sin(phi(1, i_phi)) * B_alpha(:, i_s);%bata(1, i_s) *
+	end
+end
+
 figure('Name','cell_path');
+%surf(T(:, :, 1), T(:, :, 2), T(:, :, 3));
+mesh(T(:, :, 1), T(:, :, 2), T(:, :, 3), 'FaceAlpha',0);
+hold on
+
+
 plot3(alpha_x_spec, alpha_y_spec, alpha_z_spec, 'o'...
 	, alpha_x, alpha_y, alpha_z, 'r' ...
 	, p_x, p_y, p_z, 'b');
+hold on
+
+len = 0.1;
+
+for idx_s = 1 : 10: n_s
+	ori = alpha_s(:, idx_s);
+	t = T_alpha(:, idx_s);
+	n = N_alpha(:, idx_s);
+	b = B_alpha(:, idx_s);
+	p_t = ori + t;
+	p_n = ori + n;
+	p_b = ori + b;
+	hold on
+	vectarrow(ori, p_t, 0.1, 'r');
+	hold on
+	vectarrow(ori, p_n, 0.1, 'g');
+	hold on
+	vectarrow(ori, p_b, 0.1, 'b');
+end
+
 
 
 
--- ff_spline.m
+++ ff_spline.m
@@ -3,22 +3,23 @@ function [T_a, N_a, B_a] = ff_spline(pp_a_x, pp_a_y, pp_a_z, s_spec, s)
 	T_a = zeros(3, n_s);
 	N_a = zeros(3, n_s);
 	B_a = zeros(3, n_s);
+    epsilon = 0.0001;
 	for i_s = 1 : n_s
 		s_i = s(1, i_s);
 		[~, n_spec] = size(s_spec);
 		i = 0;
 		for i_spec = 1: n_spec - 1
 			if s_spec(1, i_spec) <= s_i ...
-				&& 			 s_i <= s_spec(1, i_spec + 1)
+				&& 			 s_i < s_spec(1, i_spec + 1)
 				i = i_spec;
 				break;
 			end
 		end
-		assert(0 ~= i);
 
-		coefs_x = pp_a_x.coefs;
-		coefs_y = pp_a_y.coefs;
-		coefs_z = pp_a_z.coefs;
+
+		coefs_x = pp_a_x.coefs(i, :);
+		coefs_y = pp_a_y.coefs(i, :);
+		coefs_z = pp_a_z.coefs(i, :);
 		s_l = s_i - s_spec(i);
 		a_prime_x = 3 * coefs_x(1) * s_l^2 ...
 					+ 2 * coefs_x(2) * s_l^1 ...
@@ -38,8 +39,12 @@ function [T_a, N_a, B_a] = ff_spline(pp_a_x, pp_a_y, pp_a_z, s_spec, s)
 		a_prime_prime_z = 3 * 2 * coefs_z(1) * s_l ...
 						+ 2 * 1 * coefs_z(2);
 		a_prime_prime = [a_prime_prime_x, a_prime_prime_y, a_prime_prime_z];
-		N_a_i = (a_prime_prime/norm(a_prime_prime))';
-		B_a_i = cross(T_a_i, N_a_i);
+
+		B_a_i_scaled = cross(a_prime, a_prime_prime);
+		B_a_i = (B_a_i_scaled/norm(B_a_i_scaled))';
+		dot_product = dot(T_a_i, B_a_i);
+		assert(-epsilon < dot_product && dot_product < epsilon)
+		N_a_i = cross(B_a_i, T_a_i);
 		T_a(:, i_s) = T_a_i;
 		N_a(:, i_s) = N_a_i;
 		B_a(:, i_s) = B_a_i;
