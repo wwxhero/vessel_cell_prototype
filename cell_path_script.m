c5d41951d4b072361b92e25ee9702b84b96a9811
wanxwang <wanxin-wang@uiowa.edu>
2020-07-11 20:06:07 -0500

    random parameters k for cells trajectory

--- cell_path_script.m
+++ cell_path_script.m
@@ -14,32 +14,15 @@ alpha_y = ppval(pp_a_y, s);
 alpha_z = ppval(pp_a_z, s);
 bata 	= ppval(pp_b, s);
 
-rk		= bata .* abs(cos(s/2));
-rk = rk - 0.01;
-thetak	= 2 * s;
+
 
 [T_alpha, N_alpha, B_alpha] = ff_spline(pp_a_x, pp_a_y, pp_a_z, s_spec, s);
-
-p_x 	= alpha_x + rk .* cos(thetak) .* N_alpha(1) + rk .* sin(thetak) .* B_alpha(1);
-p_y 	= alpha_y + rk .* cos(thetak) .* N_alpha(2) + rk .* sin(thetak) .* B_alpha(2);
-p_z 	= alpha_z + rk .* cos(thetak) .* N_alpha(3) + rk .* sin(thetak) .* B_alpha(3);
 
 [~, n_s] = size(s);
 alpha_s = zeros(3, n_s);
 alpha_s(1, :) = alpha_x;
 alpha_s(2, :) = alpha_y;
 alpha_s(3, :) = alpha_z;
-p_s	= zeros(3, n_s);
-p_s(1, :) = p_x;
-p_s(2, :) = p_y;
-p_s(3, :) = p_z;
-
-d_alpha_p = zeros(1, n_s);
-err = zeros(1, n_s);
-for i_s = 1 : n_s
-	d_alpha_p(1, i_s) = norm(alpha_s(:, i_s) - p_s(:, i_s));
-	err(1, i_s) = (bata(1, i_s) - d_alpha_p(1, i_s)); %make sure the cell is in vessel
-end
 
 phi = 0:0.1:2*pi+0.1;
 [S,PHI] = meshgrid(s, phi);
@@ -56,13 +39,26 @@ end
 figure('Name','cell_path');
 %surf(T(:, :, 1), T(:, :, 2), T(:, :, 3));
 mesh(T(:, :, 1), T(:, :, 2), T(:, :, 3), 'FaceAlpha',0);
-hold on
 
 
-plot3(alpha_x_spec, alpha_y_spec, alpha_z_spec, 'o'...
-	, alpha_x, alpha_y, alpha_z, 'r' ...
-	, p_x, p_y, p_z, 'b');
-hold on
+for k = 1 : 10
+	k_1_1 = random('Uniform', -1, +1);
+	k_1_2 = random('Uniform', -pi, +pi);
+	k_2_1 = random('Uniform', -1, +1);
+	k_2_2 = random('Uniform', -pi, +pi);
+	rk		= bata .* abs(cos(k_2_1 * s + k_2_2));
+	rk = rk - 0.01;
+	thetak	= k_1_1 * s + k_1_2;
+
+	p_x 	= alpha_x + rk .* cos(thetak) .* N_alpha(1) + rk .* sin(thetak) .* B_alpha(1);
+	p_y 	= alpha_y + rk .* cos(thetak) .* N_alpha(2) + rk .* sin(thetak) .* B_alpha(2);
+	p_z 	= alpha_z + rk .* cos(thetak) .* N_alpha(3) + rk .* sin(thetak) .* B_alpha(3);
+
+	hold on
+	plot3(alpha_x_spec, alpha_y_spec, alpha_z_spec, 'o'...
+		, alpha_x, alpha_y, alpha_z, 'r' ...
+		, p_x, p_y, p_z, 'b');
+end
 
 len = 0.1;
 
@@ -75,21 +71,9 @@ for idx_s = 1 : 10: n_s
 	p_n = ori + n;
 	p_b = ori + b;
 	hold on
-	vectarrow(ori, p_t, 0.1, 'r');
+	vectarrow(ori, p_t, len, 'r');
 	hold on
-	vectarrow(ori, p_n, 0.1, 'g');
+	vectarrow(ori, p_n, len, 'g');
 	hold on
-	vectarrow(ori, p_b, 0.1, 'b');
+	vectarrow(ori, p_b, len, 'b');
 end
-
-
-
-
-figure('Name','path_width');
-bata_u = bata;
-bata_l = -bata_u;
-plot(s, bata_u, 'r' ...
-    , s, bata_l, 'r' ...
-    , s, d_alpha_p, 'b');
-
-
